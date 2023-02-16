43f5ceca069f390f1af30cae989438f1234916e6
Busi1991 <busisiwemkhwanazi65@gmail.com>
2023-02-16 15:39:50 -0800

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,10 +9,11 @@ int main(void)
 {
     int x;
 
-    x = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+    x = strlen(
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 
     write(2,
-        "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-        x);
+                "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+        , x);
     return (1);
 }

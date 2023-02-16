b532f504527a198f981c41f567b8f7e32f3e958a
Busi1991 <busisiwemkhwanazi65@gmail.com>
2023-02-16 14:38:48 -0800

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,15 +4,17 @@
 
  /**
 
-*main - Entry point
+*main - A c program that printsa line to the standard error
 
-*Return: Always 1
+*Return 1 (success)
 
 */
 
 int main(void)
 
-{write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+{
+
+              fput("and that piece of art is useful\"  - Dora Korpar, 2015-10-19\n", stdout);
 
 return (1);
 

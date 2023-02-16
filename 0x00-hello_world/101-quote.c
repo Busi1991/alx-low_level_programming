98db7878c7eab615fe56331b426a2bf9d5b743e0
Busi1991 <busisiwemkhwanazi65@gmail.com>
2023-02-16 15:06:56 -0800

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,21 +1,18 @@
 #include <stdio.h>
-
+#include <string.h>
 #include <unistd.h>
-
- /**
-
-*main - A c program that printsa line to the standard error
-
-*Return 1 (success)
-
-*/
-
+/**
+ main - main function prints to stderr
+ Return: 1 always
+ */
 int main(void)
-
 {
+    int x;
 
-              fputs("and that piece of art is useful\"  - Dora Korpar, 2015-10-19\n", stderr);
+    x = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 
-return (1);
-
+    write(2,
+        "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+        x);
+    return (1);
 }

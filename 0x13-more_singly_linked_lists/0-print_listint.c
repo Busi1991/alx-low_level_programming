a1eb27a9e8d603e2d2d61cd552d0ece57a76d9f9
Busi1991 <busisiwemkhwanazi65@gmail.com>
2023-04-04 01:02:33 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,14 +9,14 @@
 
 size_t print_listint(const listint_t *p)
 {
- size_t num = 0;
+size_t num = 0;
 
- while (p)
- {
- printf("%d\n", p->n);
- num++;
- p = p->next;
- }
+while (p)
+{
+printf("%d\n", p->n);
+num++;
+p = p->next;
+}
 
- return (num);
+return (num);
 }

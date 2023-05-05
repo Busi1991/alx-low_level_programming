18bf077f2a966791c6a681153ec6cc429afc9149
Busi1991 <busisiwemkhwanazi65@gmail.com>
2023-05-05 07:10:56 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,10 +7,11 @@
  * Return: the converted number, or 0 if there is one or more chars in the string
  * b that is not 0 or 1 or if b is NULL.
  */
+	
 unsigned int binary_to_uint(const char *b)
 {
 unsigned int result = 0;
-if (b == NULL)
+if (!b)
 return (0);
 while (*b != '\0')
 {

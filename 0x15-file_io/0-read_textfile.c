13058da01fa6257db9326cf490f941fcca30833f
Busi1991 <busisiwemkhwanazi65@gmail.com>
2023-05-08 09:38:16 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -5,42 +5,45 @@
 #include <stdlib.h>
 #include "main.h"
 
+/**
+ * read_textfile - Reads a text file and prints it to the POSIX standard output.
+ * @filename: The name of the file to read.
+ * @letters: The number of letters to read and print.
+ *
+ * Return: The actual number of letters read and printed.
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    if (filename == NULL)
-        return 0;
-
-    int fd = open(filename, O_RDONLY);
-    if (fd == -1)
-        return 0;
-
-    char *buffer = (char *)malloc((letters + 1) * sizeof(char));
-    if (buffer == NULL)
-    {
-        close(fd);
-        return 0;
-    }
-
-    ssize_t bytesRead = read(fd, buffer, letters);
-    if (bytesRead == -1)
-    {
-        free(buffer);
-        close(fd);
-        return 0;
-    }
-
-    buffer[bytesRead] = '\0';  /* Add null terminator at the end of the read text */
-
-    ssize_t bytesWritten = write(STDOUT_FILENO, buffer, bytesRead);
-    if (bytesWritten == -1 || bytesWritten != bytesRead)
-    {
-        free(buffer);
-        close(fd);
-        return 0;
-    }
-
-    free(buffer);
-    close(fd);
-
-    return bytesRead;
+int fd;
+ssize_t bytesRead, bytesWritten;
+char *buffer;
+if (filename == NULL)
+return (0);
+fd = open(filename, O_RDONLY);
+if (fd == -1)
+return (0);
+buffer = malloc((letters + 1) * sizeof(char));
+if (buffer == NULL)
+{
+close(fd);
+return (0);
+}
+bytesRead = read(fd, buffer, letters);
+if (bytesRead == -1)
+{
+free(buffer);
+close(fd);
+return (0);
+}
+buffer[bytesRead] = '\0'; /* Add null terminator at the end of the read text */
+bytesWritten = write(STDOUT_FILENO, buffer, bytesRead);
+if (bytesWritten == -1 || bytesWritten != bytesRead)
+{
+free(buffer);
+close(fd);
+return (0);
+}
+free(buffer);
+close(fd);
+return (bytesRead);
 }

8303da1536019386a85fa5b89813933fa2a095f4
Busi1991 <busisiwemkhwanazi65@gmail.com>
2023-05-08 09:18:49 -0700

    main.h update

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,9 +2,8 @@
 #define MAIN_H
 
 #include <sys/types.h>
-#include <unistd.h>  /* Include this header for ssize_t */
+#include <unistd.h>
 
-/* Function prototypes */
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);

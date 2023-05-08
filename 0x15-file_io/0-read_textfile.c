f0fea2a91a3f85e5bbeb71640161a86ffb3e05bb
Busi1991 <busisiwemkhwanazi65@gmail.com>
2023-05-08 08:39:21 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,43 +3,55 @@
 #include "main.h"
 
 /**
-* read_textfile - Reads a text file and prints it to the POSIX standard output.
-* @filename: The name of the file to read.
-* @letters: The number of letters to read and print.
-*
-* Return: The actual number of letters read and printed,
-* or 0 if the file cannot be opened or read, or if there are errors.
-*/
+ * read_textfile - Reads a text file and prints it to the POSIX standard output.
+ * @filename: The name of the file to read.
+ * @letters: The number of letters to read and print.
+ *
+ * Return: The actual number of letters read and printed,
+ *         or 0 if the file cannot be opened or read, or if there are errors.
+ */
 
 ssize_t read_textfile(const char *filename, size_t letters)
+
 {
-if (filename == NULL)
-return (0);
-FILE *file = fopen(filename, "r");
-if (file == NULL)
-return (0);
-char *buffer = (char *)malloc(letters + 1);
-if (buffer == NULL)
-{
-fclose(file);
-return (0);
-}
-ssize_t bytesRead = fread(buffer, sizeof(char), letters, file);
-if (bytesRead <= 0)
-{
-free(buffer);
-fclose(file);
-return (0);
-}
-buffer[bytesRead] = '\0';
-ssize_t bytesWritten = fwrite(buffer, sizeof(char), bytesRead, stdout);
-if (bytesWritten != bytesRead)
-{
-free(buffer);
-fclose(file);
-return (0);
-}
-free(buffer);
-fclose(file);
-return (bytesRead);
+	if (filename == NULL)
+		return (0);
+
+	FILE *file = fopen(filename, "r");
+
+	if (file == NULL)
+		return (0);
+
+	char *buffer = (char *)malloc(letters + 1);
+
+	if (buffer == NULL)
+	{
+		fclose(file);
+		return (0);
+	}
+
+	ssize_t bytesRead = fread(buffer, sizeof(char), letters, file);
+
+	if (bytesRead <= 0)
+	{
+		free(buffer);
+		fclose(file);
+		return (0);
+	}
+
+	buffer[bytesRead] = '\0';
+
+	ssize_t bytesWritten = fwrite(buffer, sizeof(char), bytesRead, stdout);
+
+	if (bytesWritten != bytesRead)
+	{
+		free(buffer);
+		fclose(file);
+		return (0);
+	}
+
+	free(buffer);
+	fclose(file);
+
+	return (bytesRead);
 }
